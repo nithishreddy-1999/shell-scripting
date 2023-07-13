6a1182e6fe1691f5e82a9491ca7288b569ab29e1
nithishreddy-1999 <nithishreddyboddireddy@gmail.com>
2023-07-13 14:47:51 -0500

    test commit

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#prompting the user for name:
+read -p "Enter your name: " name
+
+#Get date and time
+
+
+
+date=$(date)
+
+#print greeting
+
+echo "HEllo, $name! The current date and time is: $date"
+ 
